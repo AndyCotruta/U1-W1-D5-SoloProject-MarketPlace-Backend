88ea0913aa52935714a6410c7977222e186fc36a
AndyCotruta <acotruta18@gmail.com>
2023-01-10 16:54:39 +0100

    1

--- src/errorHandlers.js
+++ src/errorHandlers.js
@@ -2,9 +2,7 @@ export const badRequestHandler = (err, req, res, next) => {
   //function that takes 4 arguments and handles the type of error
   if (err.status === 400) {
     // if error has status 400 do sth
-    res
-      .status(400)
-      .send({ message: err.message, list: err.errorsList.map((e) => e.msg) }); // we manage the error status and link a message
+    res.status(400).send({ message: err.message }); // we manage the error status and link a message
   } else {
     next(err); // if the error is not 400 jump to the next error handler
   }
