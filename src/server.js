ab2634e92172e236b6642d90f3e3a22f2c9ce60a
AndyCotruta <acotruta18@gmail.com>
2023-01-10 13:32:50 +0100

    added cors opts

--- src/server.js
+++ src/server.js
@@ -10,11 +10,28 @@ import {
 import filesRouter from "./files/index.js";
 import { publicFolderPath } from "./lib/fs-tools.js";
 import productsRouter from "./products/index.js";
+import createHttpError from "http-errors";
 
-const port = 3001;
+const port = process.env.PORT;
 const server = express();
 
-server.use(cors());
+const whitelist = [
+  process.env.FE_DEVELOPMENT_URL,
+  process.env.FE_PRODUCTION_URL,
+];
+
+const corsOpts = {
+  origin: (origin, corsNext) => {
+    console.log("Current origin: " + origin);
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      corsNext(null, true);
+    } else {
+      corsNext(createHttpError(400, `Origin ${origin} is not allowed`));
+    }
+  },
+};
+
+server.use(cors(corsOpts));
 server.use(express.json());
 server.use(express.static(publicFolderPath));
 
